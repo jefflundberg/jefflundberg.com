de416cbfd7413c55039d3fdb84db7a472c6f162e
Jeff Lundberg <git@jefflundberg.com>
2021-11-09 20:51:34 -0500

    Set background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  purge: ["src/*.liquid"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: {
+        enabled: true,
+        content: ["./src/**/*.liquid", "./src/**/*.html", "./src/**/*.md"],
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
