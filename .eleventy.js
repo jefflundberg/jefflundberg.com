0cea3ecc73447be022843fad2d59fa32ee3b7403
Jeff Lundberg <git@jefflundberg.com>
2021-11-09 21:13:14 -0500

    Setup some initial content

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,6 @@ module.exports = function (eleventyConfig) {
             input: "src",
             output: "docs",
         },
-        pathPrefix: "/jefflundberg.com/",
+        // pathPrefix: "/jefflundberg.com/",
     }
 }
