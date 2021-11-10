870c8a88d9c8126f1ac8ab4c4d9177ad7203f1f6
Jeff Lundberg <git@jefflundberg.com>
2021-11-09 20:11:36 -0500

    Initial 11ty setup

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.setTemplateFormats(["liquid"])
+
+    // copy these directories as is
+    eleventyConfig.addPassthroughCopy({ "src/assets/img": "img" })
+    eleventyConfig.addPassthroughCopy({ "src/assets/fonts": "fonts" })
+    eleventyConfig.addPassthroughCopy({ "src/assets/css": "css" })
+    eleventyConfig.addPassthroughCopy({ "src/assets/js": "js" })
+
+    return {
+        dir: {
+            input: "./src",
+            output: "./dist",
+        },
+    }
+}
